c75d646ac627bee217d6cc5e946e7a41e65a1bb6
Stefanos <sagelastos@gmail.com>
2022-04-13 18:50:39 +0200

    validate password works redirects to public "secret" page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from bottle import request, response, view, run, static_file 
+from bottle import request, response, view, run, static_file, abort
 import random
 import jwt, bottle
 from jwt.exceptions import InvalidSignatureError
-from my_persistence import createDB, save_record
+from my_persistence import createDB, save_record, evaluate_code
 from send_email import send_email
 
-db_name = 'sample.db'
+db_name = "sample.db"
 jwt_secret = "secret"
 algorithm = "HS256"
 sender_email = "satestastos@gmail.com"
@@ -20,11 +20,36 @@ createDB(db_name)
 def image():
     return static_file("mitid.png", root="./images")
 
+
 ##############################
-@app.get('/2fa')
-@view("mitid")
+@app.post("/2fa")
+@view("two_fa")
+def two_fa():
+    email = request.forms["email"]
+    code = request.forms["code"]
+    if evaluate_code(db_name, email, code):
+        return
+    else:
+        response.status = 403
+        return {
+            "code": "invalid_auth_code",
+            "description": "Wrong authorization code",
+        }
+
+
+##############################
+@app.get("/2fa")
+@view("two_fa")
 def two_fa():
     return
+
+
+##############################
+@app.get("/secret_page")
+@view("secret_page")
+def two_fa():
+    return
+
 
 ##############################
 @app.get("/")
@@ -36,46 +61,52 @@ def index():
 ##############################
 @app.post("/api-login")
 def jwt_validate():
-    auth = request.headers.get('Authorization', None)
+    auth = request.headers.get("Authorization", None)
     if not auth:
         response.status = 403
         return {
-            'code': 'authorization_header_missing',
-            'description': 'Authorization header is expected'
+            "code": "authorization_header_missing",
+            "description": "Authorization header is expected",
         }
 
     parts = auth.split()
 
-    if parts[0].lower() != 'bearer':
+    if parts[0].lower() != "bearer":
         response.status = 403
         return {
-            'code': 'invalid_header',
-            'description': 'Authorization header must start with Bearer'
+            "code": "invalid_header",
+            "description": "Authorization header must start with Bearer",
         }
     elif len(parts) == 1:
         response.status = 403
-        return {'code': 'invalid_header', 'description': 'Token not found'}
+        return {"code": "invalid_header", "description": "Token not found"}
     elif len(parts) > 2:
         response.status = 403
         return {
-            'code': 'invalid_header',
-            'description': 'Authorization header must be Bearer + \s + token'
+            "code": "invalid_header",
+            "description": "Authorization header must be Bearer + \s + token",
         }
 
     try:
         print(parts[1])
         jwt.decode(parts[1], jwt_secret, algorithm)
         # TODO generate 4 digit code, send email and save email - code pair in DB
-        code = random.randint(1000,9999)
+        code = random.randint(1000, 9999)
         save_record(db_name=db_name, email=receiver_email, code=code)
-        send_email(sender_email=sender_email, password=password, receiver_email=receiver_email, random_auth_code=code)
-        return "OK"    
+        send_email(
+            sender_email=sender_email,
+            password=password,
+            receiver_email=receiver_email,
+            random_auth_code=code,
+        )
+        return "OK"
     except InvalidSignatureError:
-        print("An exception occurred") 
+        print("An exception occurred")
         response.status = 403
         return {
-            'code': 'invalid_signature',
-            'description': 'Token signature is not valid'
+            "code": "invalid_signature",
+            "description": "Token signature is not valid",
         }
+
 
 run(app, host="127.0.0.1", port=3333, debug=True, reloader=True, server="paste")
--- my_persistence.py
+++ my_persistence.py
@@ -1,23 +1,31 @@
 import sqlite3
 
+
 def createDB(db_name):
     conn = sqlite3.connect(db_name)
-    conn.execute("CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, email char(100) NOT NULL, code INTEGER(4) NOT NULL)")
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, email char(100) NOT NULL, code INTEGER(4) NOT NULL)"
+    )
+
 
 def save_record(db_name, email, code):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    c.execute("INSERT INTO records (email,code) VALUES (?,?)", (email,code))
+    c.execute("INSERT INTO records (email,code) VALUES (?,?)", (email, code))
     new_id = c.lastrowid
-    print(f'Just saved record with ID: {new_id}')
+    print(f"Just saved record with ID: {new_id}")
     conn.commit()
     c.close()
 
-def save_record(db_name, email, code):
+
+def evaluate_code(db_name, email, code):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    c.execute("INSERT INTO records (email,code) VALUES (?,?)", (email,code))
-    new_id = c.lastrowid
-    print(f'Just saved record with ID: {new_id}')
-    conn.commit()
-    c.close()
+    c.execute(
+        "SELECT id, email, code FROM records WHERE email=? AND code=?", (email, code)
+    )
+    results = c.fetchall()
+    if len(results) == 0:
+        return False
+    else:
+        return True
