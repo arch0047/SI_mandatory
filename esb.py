52963bb948ae88556a9316dee428dbf398e735fd
Stefanos <sagelastos@gmail.com>
2022-06-11 19:02:22 +0200

    changed list format

--- esb.py
+++ esb.py
@@ -1,38 +1,39 @@
-from bottle import get, run, response
-import json
-import redis
+from bottle import get, post, run, response
+import json, auth
 
-r = redis.Redis(
-    host="localhost", port=7000, db=0, password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81"
-)
+# r.hset("user:12345", mapping={"id": "1", "email": "@a", "token": "12345"})
+# r.hset("user:67890", mapping={"id": "2", "email": "@b", "token": "67890"})
 
-r.hset("user:12345", mapping={"id": "1", "email": "@a", "token": "12345"})
-r.hset("user:67890", mapping={"id": "2", "email": "@b", "token": "67890"})
-
-messages = {
-    "1": [
-        {"id": "f768ff7c-893f-48be-8afc-837ff2390f38", "message": "m1", "access": "*"},
-        {"id": "204f3ebc-c7c0-4d78-a97b-0f3b582c3d84", "message": "m2", "access": "*"},
-        {"id": "437691ff-2fe7-448c-82c6-a9fa7e42da37", "message": "m3", "access": "*"},
-        {"id": "c34993cd-7f79-40cb-97dc-a3eac4b0b37b", "message": "m4", "access": "*"},
-    ]
-}
+messages = [
+    {
+        "f768ff7c-893f-48be-8afc-837ff2390f38/recipies/french tomatoes/1654964418": {
+            "c": "Prepare this , cook that hwatever",
+            "a": "service a",
+        }
+    },
+    {
+        "f768ff7c-893f-48be-8afc-837ff2390f38/shows/french tomatoes/1654964418": {
+            "c": "Prepare this , cook that hwatever",
+            "a": "service a",
+        }
+    },
+    {
+        "f768ff7c-893f-48be-8afc-837ff2390f38/animals/french tomatoes/1654964418": {
+            "c": "Prepare this , cook that hwatever",
+            "a": "service a",
+        }
+    },
+]½
 
 ############## THIS IS ABOUT READING MESSAGES
-@get("/provider/<id>/from/<last_message_id>/limit/<limit:int>/token/<token>")
-def _(id, last_message_id, limit, token):
-
-    r.set("foo", "bar")
-
+@get("/provider/<id>/from/<last_message_id>/limit/<limit:int>")
+def _(id, last_message_id, limit):
+    auth.verify_token()
     response.content_type = "application/json"
     try:
         # validation
         if limit <= 0:
             raise Exception(f"limit cannot be {limit}")
-        # validate that the token is registered in the system
-        user = r.hexists(f"user:{token}", "id")
-        if not user:
-            raise Exception("token is invalid")
         start_index = -1
         # for message in messages[id]:
         #     print(message)
@@ -53,6 +54,12 @@ def _(id, last_message_id, limit, token):
     except Exception as ex:
         response.status = 400
         return str(ex)
+
+
+############## THIS IS ABOUT WRITING MESSAGES
+@post("/provider/<id>/from/<last_message_id>/limit/<limit:int>")
+def _(id, last_message_id, limit):
+    auth.verify_token()
 
 
 ##############
